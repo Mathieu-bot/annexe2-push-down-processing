f53538cda8010f7e43a220e93194ff1ff7b1a547
Mathieu <hei.tafita.2@gmail.com>
2026-02-19 16:20:02 +0300

    chore: add VoteSummary model

---
+++ src/main/java/org/example/model/VoteSummary.java
@@ -0,0 +1,35 @@
+package org.example.model;
+
+public class VoteSummary {
+
+    private final long validCount;
+    private final long blankCount;
+    private final long nullCount;
+
+    public VoteSummary(long validCount, long blankCount, long nullCount) {
+        this.validCount = validCount;
+        this.blankCount = blankCount;
+        this.nullCount = nullCount;
+    }
+
+    public long getValidCount() {
+        return validCount;
+    }
+
+    public long getBlankCount() {
+        return blankCount;
+    }
+
+    public long getNullCount() {
+        return nullCount;
+    }
+
+    @Override
+    public String toString() {
+        return "VoteSummary(" +
+                "validCount=" + validCount +
+                ", blankCount=" + blankCount +
+                ", nullCount=" + nullCount +
+                ")";
+    }
+}
