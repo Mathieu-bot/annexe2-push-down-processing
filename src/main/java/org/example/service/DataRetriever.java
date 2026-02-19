b63ac044608d7d203cbfcf685c54e709a34cb48c
Mathieu <hei.tafita.2@gmail.com>
2026-02-19 16:33:37 +0300

    feat: implement DataRetriever::countAllVotes that return the number of all votes

--- src/main/java/org/example/service/DataRetriever.java
+++ src/main/java/org/example/service/DataRetriever.java
@@ -1,4 +1,27 @@
 package org.example.service;
 
+import org.example.database.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DataRetriever {
+
+    private final DBConnection dbConnection = new DBConnection();
+
+    public long countAllVotes() {
+        String sql = "SELECT COUNT(*) FROM vote;";
+        try (Connection connection = dbConnection.getDBConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet rs = statement.executeQuery())
+            {
+                rs.next();
+                return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
