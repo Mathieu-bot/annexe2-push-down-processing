6a0b249fc3be5853e3713def327183cb93e82eb1
Mathieu <hei.tafita.2@gmail.com>
2026-02-25 14:27:55 +0300

    feat: implement DataRetriever::countVotesByType

--- src/main/java/org/example/service/DataRetriever.java
+++ src/main/java/org/example/service/DataRetriever.java
@@ -1,6 +1,7 @@
 package org.example.service;
 
 import org.example.database.DBConnection;
+import org.example.model.VoteType;
 import org.example.model.VoteTypeCount;
 
 import java.sql.Connection;
@@ -27,5 +28,31 @@ public class DataRetriever {
         }
     }
 
+    public List<VoteTypeCount> countVotesByType() {
+        String sql = """
+               SELECT vote_type, COUNT(vote_type) AS count
+               FROM vote
+               GROUP BY vote_type
+               ORDER BY vote_type;
+               """;
+        List results = new ArrayList<>();
 
+        try (Connection connection = dbConnection.getDBConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet rs = statement.executeQuery())
+        {
+            while (rs.next()) {
+                VoteTypeCount voteTypeCount = new VoteTypeCount(
+                        VoteType.valueOf(rs.getString(1)),
+                        rs.getLong(2)
+                );
+                results.add(voteTypeCount);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return results;
+    }
 }
+
